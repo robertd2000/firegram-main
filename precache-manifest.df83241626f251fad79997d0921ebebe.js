57939df0925a4fdb3ec33aa07f671b367534fec3
Rob <rob@example.com>
2022-04-01 20:30:59 +0300

    Updates

---
+++ precache-manifest.df83241626f251fad79997d0921ebebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f26c168d5acac771e1c7a860a5b984",
+    "url": "/firegram-main/index.html"
+  },
+  {
+    "revision": "8032a8760cb354063231",
+    "url": "/firegram-main/static/css/main.b48afc14.chunk.css"
+  },
+  {
+    "revision": "ca64ab18a96608cbe256",
+    "url": "/firegram-main/static/js/2.0153de13.chunk.js"
+  },
+  {
+    "revision": "d84df437604b0628b79cfd25b85de15e",
+    "url": "/firegram-main/static/js/2.0153de13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8032a8760cb354063231",
+    "url": "/firegram-main/static/js/main.49b05a56.chunk.js"
+  },
+  {
+    "revision": "b190cae4cdbdeffae48f",
+    "url": "/firegram-main/static/js/runtime-main.268108cc.js"
+  }
+]);
